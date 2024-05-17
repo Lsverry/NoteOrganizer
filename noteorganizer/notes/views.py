b1f4a39adacfac8f5aafd8a8b4dad20ded3da691
Luis Echeverry <nmx0040@gmail.com>
2024-05-17 07:19:27 +0000

    Add a view for user registration.

--- noteorganizer/notes/views.py
+++ noteorganizer/notes/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Note
 from .forms import NoteForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('note_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
 
 @login_required
 def note_list(request):
